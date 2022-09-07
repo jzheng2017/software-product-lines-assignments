179ea19104a5958428cc7344dc256529f37d1b15
Jiankai Zheng <J.Zheng1@student.han.nl>
2022-09-07 20:28:50 +0200

    Remove main

--- src/spl/services/SimpleEncryptionService.java
+++ src/spl/services/SimpleEncryptionService.java
@@ -1,6 +1,5 @@
 package spl.services;
 
-import static spl.Main.rot13;
 
 public class SimpleEncryptionService implements EncryptionService{
     @Override
@@ -11,5 +10,18 @@ public class SimpleEncryptionService implements EncryptionService{
     @Override
     public String decrypt(String text) {
         return new StringBuilder(rot13(text)).reverse().toString();
+    }
+
+    private String rot13(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
