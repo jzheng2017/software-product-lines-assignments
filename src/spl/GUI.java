84ca3003d61ca2c1da7584800d3af5ea506c9cd4
Jiankai Zheng <J.Zheng1@student.han.nl>
2022-09-14 18:41:42 +0200

    Update GUI main function

--- src/spl/GUI.java
+++ src/spl/GUI.java
@@ -11,8 +11,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -180,10 +178,8 @@ public class GUI {
     }
 
     public static void main(String[] args) {
-        List<String> allFeatures = Arrays.asList("reverse", "rot13", "usernamecolors");
-        List<String> arguments = new ArrayList<>(Arrays.asList(args));
-        for(String feat : allFeatures){
-            fcs.addFeature(feat.toLowerCase(), arguments.contains(feat));
+        for(String feature : args){
+            fcs.addFeature(feature.toLowerCase(), true);
         }
         chatService = new ChatService(new FileLogService(), (fcs.isFeatureOn("rot13") ? new Rot13EncryptionService() : new ReverseStringEncryptionService()));
         user = new Client(hostIP, port, fcs);
