5c945ff75bb14ec8681d11140150d6599a82db94
Dibido <dibran.dokter@ru.nl>
2022-09-19 15:00:23 +0200

    changed switch to version supported by old java version

--- src/spl/services/EncryptionType.java
+++ src/spl/services/EncryptionType.java
@@ -5,10 +5,10 @@ public enum EncryptionType {
     REVERSE;
 
     public static EncryptionType toEnum(String encryptionType) {
-        return switch (encryptionType.toLowerCase()) {
-            case "reverse" -> REVERSE;
-            case "rot13" -> ROT13;
-            default -> throw new IllegalStateException(String.format("Encryption type %s can not be converted to enum value", encryptionType));
-        };
+        switch (encryptionType.toLowerCase()) {
+            case "reverse": return REVERSE;
+            case "rot13": return ROT13;
+            default: throw new IllegalStateException(String.format("Encryption type %s can not be converted to enum value", encryptionType));
+        }
     }
 }
