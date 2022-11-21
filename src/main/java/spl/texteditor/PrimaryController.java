aaae6442e4d66970287f769e61607087f74411f0
Jiankai Zheng <jk.zheng@hotmail.com>
2022-11-21 22:41:24 +0100

    Dump

---
+++ src/main/java/spl/texteditor/PrimaryController.java
@@ -0,0 +1,15 @@
+package spl.texteditor; 
+
+import java.io.IOException; 
+import javafx.fxml.FXML; 
+
+public  class  PrimaryController {
+	
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+
+
+}
