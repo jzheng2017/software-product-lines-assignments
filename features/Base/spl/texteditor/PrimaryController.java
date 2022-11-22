2f9c00491ff953e597019209afaec3703bdabe5e
Jiankai Zheng <jk.zheng@hotmail.com>
2022-11-22 22:06:42 +0100

    Add reading and writing to file

--- features/Base/spl/texteditor/PrimaryController.java
+++ features/Base/spl/texteditor/PrimaryController.java
@@ -1,12 +1,44 @@
-package spl.texteditor;
+package spl.texteditor; 
 
-import java.io.IOException;
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import spl.texteditor.storage.FileReadWriteService;
+import spl.texteditor.storage.ReadWriteService;
 
-public class PrimaryController {
+public  class  PrimaryController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrimaryController.class);
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TextArea textArea;
+    @FXML
+    private Stage stage;
+    private ReadWriteService readWriteService = new FileReadWriteService();
+    @FXML
+    public void onOpenFileAction() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose a file you want to edit");
+        File file = fileChooser.showOpenDialog(stage);
+        textArea.setText(readWriteService.read(file.getPath()));
+    }
+
+    @FXML
+    public void onFileSave() {
+        String lastFileRead = readWriteService.lastFileRead();
+        String contents = textArea.getText();
+        if (lastFileRead == null || lastFileRead.isEmpty() || lastFileRead.isBlank()) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save");
+            File file = fileChooser.showSaveDialog(stage);
+            readWriteService.write(file.getPath(), contents);
+        } else {
+            readWriteService.write(lastFileRead, contents);
+        }
     }
 }
---
+++ src/main/java/spl/texteditor/storage/ReadWriteService.java
@@ -0,0 +1,19 @@
+package spl.texteditor.storage; 
+
+public  interface  ReadWriteService {
+	
+
+    String read(String identifier);
+
+	
+    void write(String identifier, String content);
+
+	
+
+    String lastFileRead();
+
+	
+    String lastFileWritten();
+
+
+}
