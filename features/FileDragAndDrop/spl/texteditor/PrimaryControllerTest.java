b97f43e4bc7753e7ecbde73bdc72a91444451e65
Rick ten Tije <33488894+GromperEUW@users.noreply.github.com>
2022-12-10 14:59:14 +0100

    added unit test dependency

---
+++ features/FileDragAndDrop/spl/texteditor/PrimaryControllerTest.java
@@ -0,0 +1,27 @@
+package spl.texteditor;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.event.EventTarget;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.input.PickResult;
+import javafx.scene.input.InputEvent;
+
+import java.io.File;
+
+class PrimaryControllerTest {
+
+	@Test
+	void testOnDragOver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOnDragDropped() {
+		fail("Not yet implemented");
+	}
+
+}
